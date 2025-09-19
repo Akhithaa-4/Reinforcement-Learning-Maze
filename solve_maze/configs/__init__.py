8b711e2c6068cfae48258e8691b05171045635b2
Akhitha P <156596257+Akhithaa-4@users.noreply.github.com>
2025-09-19 10:35:57 +0530

    Delete solve_maze directory

--- solve_maze/configs/__init__.py
+++
@@ -1,5 +0,0 @@
-from .default_cfg import defaultCFG as cfg
-
-###################################################
-# init cfg to be class with default configuration #
-###################################################
--- solve_maze/run.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-IF "%~1"=="" (
-    python solve_maze.py -config_file config.yaml
-) ELSE (
-    python solve_maze.py -config_file config.yaml ALGORITHM.NAME %1 MAZE.SIZE %2
-)
